510c976288ccbabdade3d7d0e733ebb307abcc5e
Yassinsasso <yassinyamen121@gmail.com>
2025-12-12 23:53:54 +0200

    Adding navbar

--- travel-website/routes/destination.js
+++ travel-website/routes/destination.js
@@ -4,38 +4,70 @@ const { connect } = require("../database");
 
 // ---------- DESTINATION PAGE ----------
 router.get("/destination/:name", async (req, res) => {
-    const db = await connect();
-    const destinations = db.collection("destinations");
+    try {
+        const db = await connect();
+        const destinations = db.collection("destinations");
 
-    const name = req.params.name;
-    const dest = await destinations.findOne({ name });
+        const name = req.params.name;
+        const dest = await destinations.findOne({ name });
 
-    res.render("destination", {
-        destination: dest,
-        error: null
-    });
+        if (!dest) {
+            return res.status(404).send("Destination not found");
+        }
+
+        res.render("destination", {
+            destination: dest,
+            error: null
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server error");
+    }
 });
 
 // ---------- ADD TO WANT-TO-GO ----------
 router.post("/destination/:name/add", async (req, res) => {
-    const db = await connect();
-    const users = db.collection("myCollection");
+    try {
+        const db = await connect();
+        const users = db.collection("myCollection");
+        const destinations = db.collection("destinations");
 
-    const username = req.session.user.username;
-    const destName = req.params.name;
+        // Check if user is logged in
+        if (!req.session.user) {
+            return res.redirect("/login");
+        }
 
-    const user = await users.findOne({ username });
+        const username = req.session.user.username;
+        const destName = req.params.name;
 
-    if (user.wantToGo.includes(destName)) {
-        return res.render("destination", { destination: { name: destName }, error: "Already in list." });
+        // Find user and destination
+        const user = await users.findOne({ username });
+        const dest = await destinations.findOne({ name: destName });
+
+        if (!dest) {
+            return res.status(404).send("Destination not found");
+        }
+
+        // Check if already in wantToGo
+        if (user.wantToGo.includes(destName)) {
+            return res.render("destination", {
+                destination: dest,
+                error: "Already in your Want-to-Go list."
+            });
+        }
+
+        // Add to wantToGo
+        await users.updateOne(
+            { username },
+            { $push: { wantToGo: destName } }
+        );
+
+        // Redirect back to the same destination page
+        res.redirect(`/destination/${destName}`);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server error");
     }
-
-    await users.updateOne(
-        { username },
-        { $push: { wantToGo: destName } }
-    );
-
-    res.redirect("/want-to-go");
 });
 
 module.exports = router;
